7f283085239a453dd48abd6a7c3f5e7dcb316399
jkrabogne <jkrabogne02@gmail.com>
2020-08-30 13:47:03 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Sample</title>
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css">
+	</head>
+	<body>
+		<?php include "index/navbar.html"; ?>
+		<div id = "home-banner">
+			<?php include "index/home-banner.html"; ?>
+		</div>
+		<div id = "section-1" class = "container">
+			<?php include "index/section-1.html"; ?>
+		</div>
+		<div id = "section-2" class = "container">
+			<?php include "index/section-2.html"; ?>		
+		</div>
+		<div id = "section-3" class = "container">
+			<?php include "index/section-3.html"; ?>		
+		</div>
+		<a href = "#home-banner" class = "back-to-top">Back to Top</a>
+	</body>
+</html>
